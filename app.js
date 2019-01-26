e180babe5d40110fd5008945c936bcb563f4b674
maximkoshelenko <qazwsx741852>
2019-01-27 00:30:37 +0200

    Init commit

---
+++ app.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const passport = require('passport');
+
+// Load User Model
+require('./models/User')
+
+// Passport Config
+require('./config/passport')(passport);
+
+// Load Routes
+const auth = require('./routes/auth');
+const index = require('./routes/indexTwo');
+const stories = require('./routes/stories');
+
+// Load keys
+
+const keys = require('./config/keys');
+
+// // Map global promise - get rid of warning
+mongoose.Promise = global.Promise;
+
+// Mongoose connect
+mongoose.connect(keys.mongoURI, {
+    useMongoClient: true
+})
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
+
+const app = express();
+
+//Handlebars Middleware
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+//Express session middleware
+app.use(cookieParser());
+app.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false
+}));
+
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Set global vars
+app.use((req, res, next) => {
+    res.locals.user = req.user || null;
+    next();
+});
+
+//Use Routes
+app.use('/stories', stories);
+app.use('/auth', auth);
+app.use('/', index);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI:'mongodb://ocetp1:vfrcbv123@ds161724.mlab.com:61724/storybooks-dev',
+    googleClientID:'734691384010-crmcpvb91ttfckvaq2dnfogo2kqds38n.apps.googleusercontent.com',
+    googleClientSecret:'WUzXd43gZik2g40ACaxtNk72'
+}
