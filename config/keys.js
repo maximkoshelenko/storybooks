5d73e6aca8c5d2a986dca840f3a84a03b1c5af89
maximkoshelenko <qazwsx741852>
2019-02-14 23:09:25 +0200

    Second project is done :)

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-    mongoURI:'mongodb://ocetp1:vfrcbv123@ds161724.mlab.com:61724/storybooks-dev',
-    googleClientID:'734691384010-crmcpvb91ttfckvaq2dnfogo2kqds38n.apps.googleusercontent.com',
-    googleClientSecret:'WUzXd43gZik2g40ACaxtNk72'
+if (process.env.NODE_ENV === 'production') {
+    module.exports = require('./keys_prod');
+} else {
+    module.exports = require('./keys_dev');
 }
